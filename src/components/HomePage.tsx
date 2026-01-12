ecbe198b0001eb1fdd28bd7811d9be97963466ea
Sebika Nepal <sebika.nepal@ebpearls.com>
2026-01-12 16:38:29 +0545

    added a middleware

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,38 @@
 "use client";
-import { useSession } from "next-auth/react";
+import { Button } from "@mui/material";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const HomePage = () => {
   const { data: session } = useSession();
-  console.log("🚀 ~ HomePage ~ data:", session);
-  return <div>HomePage</div>;
+  const router = useRouter();
+
+  return (
+    <>
+      <div>HomePage</div>
+      {session ? (
+        <Button
+          variant="contained"
+          onClick={() => {
+            signOut();
+            router.push("/login");
+          }}
+        >
+          Sign Out
+        </Button>
+      ) : (
+        <Button
+          variant="contained"
+          onClick={() => {
+            router.push("/login");
+          }}
+        >
+          Sign In
+        </Button>
+      )}
+    </>
+  );
 };
 
 export default HomePage;
