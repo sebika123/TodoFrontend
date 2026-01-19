62e510e8693d407c8526d1c3afe4158c9e94b560
Sebika Nepal <sebika.nepal@ebpearls.com>
2026-01-19 16:07:28 +0545

    Added middleware to check whether authenticated or not

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,6 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = req.nextUrl;
-  console.log("🚀 ~ middleware ~ pathname:", pathname);
-
-  console.log("Middleware token:", token);
 
   // If user IS logged in and tries login/register
   if (token && (pathname === "/login" || pathname === "/register")) {
